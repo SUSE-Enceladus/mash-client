60bdd5598011c0eb7562f261b6d0143f10c45669
Sean Marlow <sean.marlow@suse.com>
2020-03-25 10:13:05 -0500

    Add commands for password change and reset.
    Used for password based logins to Mash server.

--- tests/test_user.py
+++ tests/test_user.py
@@ -73,3 +73,54 @@ def test_user_delete(mock_requests, mock_time):
     )
     assert result.exit_code == 0
     assert 'User deleted' in result.output
+
+
+@patch('mash_client.cli_utils.time')
+@patch('mash_client.cli_utils.requests')
+def test_user_password_reset(mock_requests, mock_time):
+    """Test mash user password reset."""
+    response = Mock()
+    response.status_code = 200
+    response.json.return_value = {
+        'msg': 'Password reset submitted. An email '
+               'will be sent with steps to change your password.'
+    }
+    mock_requests.post.return_value = response
+    mock_time.time.return_value = 1568150480
+
+    runner = CliRunner()
+    result = runner.invoke(
+        main,
+        [
+            '-C', 'tests/data/', 'user', 'password', 'reset'
+        ],
+        input='y\n'
+    )
+    assert result.exit_code == 0
+    assert 'Password reset submitted' in result.output
+
+
+@patch('mash_client.cli_utils.time')
+@patch('mash_client.cli_utils.requests')
+def test_user_password_change(mock_requests, mock_time):
+    """Test mash user password change."""
+    response = Mock()
+    response.status_code = 200
+    response.json.return_value = {
+        'msg': 'Password changed successfully. You can now login.'
+    }
+    mock_requests.put.return_value = response
+    mock_time.time.return_value = 1568150480
+
+    runner = CliRunner()
+    result = runner.invoke(
+        main,
+        [
+            '-C', 'tests/data/', 'user', 'password', 'change'
+        ],
+        input='oldpassword\n'
+              'secretpassword\n'
+              'secretpassword\n'
+    )
+    assert result.exit_code == 0
+    assert 'Password changed successfully' in result.output
