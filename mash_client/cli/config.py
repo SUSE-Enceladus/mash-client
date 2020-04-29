b154866b6e2451cf8ec3e35ccfbf9c78da6913ef
Robert Schweikert <rjschwei@suse.com>
2020-04-29 14:35:17 -0400

    - Improvements to config file setup   + Show the default config directory in the prompt   + Create the config directory if it does not exists   + Support the use of "~" as shortcut to the home directory

--- mash_client/cli/config.py
+++ mash_client/cli/config.py
@@ -44,7 +44,7 @@ def config():
 @click.option(
     '--config-dir',
     type=click.STRING,
-    default='',
+    default='~/.config/mash_client/',
     show_default=True,
     prompt='Enter Mash client config directory',
     help='Mash client config directory to use. It is recommended that this'
@@ -150,6 +150,10 @@ def setup_config(
 
     if not config_dir:
         config_dir = default_config_dir
+
+    config_dir = os.path.expanduser(config_dir)
+    if not os.path.isdir(config_dir):
+        os.makedirs(config_dir)
 
     config_file_path = os.path.join(config_dir, profile + '.yaml')
 
