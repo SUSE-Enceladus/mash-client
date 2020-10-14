6bb7afbf7f6168375530df0e5e8bf360de885e35
Sean Marlow <sean.marlow@suse.com>
2020-10-14 15:51:07 -0500

    Add raise_for_status option.
    - Option defaults to True for CLI usage.
    - Simplify the raise for status clauses. Raise based on keys in
      response rather than the response code.
    - Add raise_for_status option to get_job_schema_by_cloud.

--- mash_client/cli_utils.py
+++ mash_client/cli_utils.py
@@ -150,7 +150,8 @@ def handle_request(
     endpoint,
     job_data=None,
     action='post',
-    token=None
+    token=None,
+    raise_for_status=True
 ):
     """
     Post request based on endpoint and data.
@@ -181,31 +182,26 @@ def handle_request(
             '{url}'.format(url=config_data['url'])
         )
 
-    if response.status_code in (200, 201):
-        return response.json()
-    elif response.status_code == 400:
-        try:
-            error = '\n'.join(response.json()['errors'].values())
-        except KeyError:
-            error = response.json()['msg']
-
-        raise MashClientException(error)
-    elif response.status_code == 401:
+    try:
+        result = response.json()
+    except json.decoder.JSONDecodeError:
         raise MashClientException(
-            response.json()['msg'] + '. Please login again.'
+            'The requested URL was not found on the server: {url}'.format(
+                url=''.join([config_data['url'], endpoint])
+            )
         )
-    elif response.status_code == 404:
-        try:
-            msg = response.json()['msg']
-        except (json.decoder.JSONDecodeError, KeyError):
-            msg = 'The requested URL was not found on the server:' \
-                  ' {url}'.format(
-                      url=''.join([config_data['url'], endpoint])
-                  )
 
-        raise MashClientException(msg)
-    elif response.status_code in (403, 409):
-        raise MashClientException(response.json()['msg'])
+    if not raise_for_status or response.status_code in (200, 201):
+        return result
+    elif 'errors' in result:
+        raise MashClientException(
+            '\n'.join(
+                '{key}: {val}'.format(key=key, val=val)
+                for key, val in result['errors'].items()
+            )
+        )
+    elif 'msg' in result:
+        raise MashClientException(result['msg'])
     else:
         response.raise_for_status()
 
@@ -214,7 +210,8 @@ def handle_request_with_token(
     config_data,
     endpoint,
     job_data=None,
-    action='post'
+    action='post',
+    raise_for_status=True
 ):
     """
     Submit request to API with access token.
@@ -243,7 +240,8 @@ def handle_request_with_token(
         endpoint,
         job_data=job_data,
         action=action,
-        token=tokens['access_token']
+        token=tokens['access_token'],
+        raise_for_status=raise_for_status
     )
 
     return result
@@ -435,12 +433,21 @@ def get_annotated_property(key, value, required):
     return annotated_value
 
 
-def get_job_schema_by_cloud(config_data, output_style, cloud):
+def get_job_schema_by_cloud(
+    config_data,
+    output_style,
+    cloud,
+    raise_for_status=True
+):
     result = handle_request(
         config_data,
         '/jobs/{cloud}/'.format(cloud=cloud),
-        action='get'
+        action='get',
+        raise_for_status=raise_for_status
     )
+
+    if 'properties' not in result:
+        return result
 
     if output_style == 'json':
         json_result = {}
